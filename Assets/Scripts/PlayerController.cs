6aff9a706dc72679cb08aedc463ce7a1f95e7684
daltonbr <sacinopatinete@gmail.com>
2016-05-16 20:54:40 -0300

    Add TestScene, basic Player and basic Assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+
+	void Update () {
+		Vector2 velocity = this.GetComponent<Rigidbody2D>().velocity;
+		if (Input.GetButtonDown("Jump")){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x, jumpHeight);
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight);
+			Debug.Log("My jump velocity when I jump is: " + jumpHeight);
+		}
+
+		float inputX = (Input.GetAxisRaw("Horizontal"));
+		GetComponent<Rigidbody2D>().velocity = new Vector2(inputX * moveSpeed, velocity.y);
+
+	}
+}
