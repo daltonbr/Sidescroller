eb14d9e5900b4bbbaf031e1749d42373d4fbdc5f
daltonbr <sacinopatinete@gmail.com>
2016-05-24 15:27:21 -0300

    Player not sticking in the walls and not moving after death

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,28 +3,30 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+	public GameObject player;
 	public CheckpointController checkpointController;
 	public PlayerController playerController;
-	//public GameObject player;
 	private Health health;
 	public GameObject respawnParticle;
 
 	public float respawnDelay;
 
 	void Start () {
-		health = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>(); 
-		playerController = FindObjectOfType<PlayerController>();
+		player = GameObject.FindGameObjectWithTag("Player");
+		health = player.GetComponent<Health>(); 
+		playerController = player.GetComponent<PlayerController>();
 		checkpointController = FindObjectOfType<CheckpointController>();
 	}
 
 	public void RespawnPlayer() {
 		playerController.GetComponent<SpriteRenderer>().enabled = false;
 		playerController.enabled = false;
+
 		StartCoroutine("RespawnPlayerCo");
 	}
 
 	public IEnumerator RespawnPlayerCo() {
-		
+		player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 		yield return new WaitForSeconds(respawnDelay);
 		Debug.Log("Player Respawn");
 		health.ResetHealth();
