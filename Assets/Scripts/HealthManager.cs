3a3f31332d59d8ae7c49956eb29b5383a69c659c
daltonbr <sacinopatinete@gmail.com>
2016-12-20 15:20:59 -0200

    imported 5.5.0f3 and reorganized some assets

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour {
+
+	private int health;
+	public Text healthText;
+
+	void Start () {
+		//healthText = GetComponent<Text>();
+		if (healthText == null) Debug.LogError("scoreText not founded! Please refer a text object to ScoreManager Script!");
+		health = 100;
+	}
+
+	void FixedUpdate () {
+		if (health < 0) {health = 0;}
+		UpdateHealth();
+	}
+
+	public void ChangeHealth(int pointsToChange) {
+		health += pointsToChange;
+		UpdateHealth();
+	}
+
+	public void SetHealth(int valueToSet) {
+		health = valueToSet;
+	}
+
+	public void UpdateHealth() {
+		healthText.text = "" + health;
+	}
+
+//	public void UpdateGUI() {
+//	}
+}
