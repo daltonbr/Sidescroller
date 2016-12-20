a6e98a540fe5e029329a3d00557707bb4219556c
daltonbr <sacinopatinete@gmail.com>
2016-12-20 15:21:51 -0200

    add Spring

---
+++ Assets/Scripts/LevelMechanics/Spring.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour {
+
+	public AudioSource springSound;
+	public float springPower = 1000f;
+	private Animator springAnimator;
+
+	void Awake()
+	{
+		springAnimator = this.GetComponent<Animator>();
+		if (!springAnimator) Debug.LogError("An Animator was not founded on " + this.gameObject.name);
+		springSound = this.GetComponent<AudioSource>();
+		if (!springSound) Debug.LogError("An AudioSource was not founded on " + this.gameObject.name);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player")
+			springAnimator.SetTrigger("released");
+			//Debug.Log(coll.gameObject.name + " has collided with " + this.gameObject.name);
+			coll.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * springPower);
+			springSound.Play();
+	}
+}
